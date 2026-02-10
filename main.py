94eb358ca355863126b3b5270e81b98c132a6cb5
Alex <abg523@york.ac.uk>
2026-02-10 16:28:10 +0000

    commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import model
 import torch
 from torch.utils.data import DataLoader
@@ -51,60 +50,4 @@ if __name__ == '__main__':
                 print('The model predicts a positive review')
             else:
                 print('The model predicts a negative review')
-
-=======
-import model
-import torch
-from torch.utils.data import DataLoader
-from torch import nn
-import nltk
-nltk.download('punkt_tab')
-from nltk import word_tokenize
-import pickle
-import re
-
-
-with open('word_bank.pkl', 'rb') as f:
-    word_bank = pickle.load(f)
-def data_pipeline(review):
-    remove_html = lambda x : re.sub(r'<.*?>', '', x).lower()
-    review = remove_html(review)
-    # Simple function to prepare review for mopel use
-    tokens = word_tokenize(review)
-    output = []
-    for token in tokens:
-        if token in word_bank.keys():
-            output.append(word_bank[token])
-        else:
-            output.append(0)
-    n = len(output)
-    output = torch.tensor(output).reshape(1, n)
-    return output
-        
-
-if __name__ == '__main__':
-    PATH = 'model.pt'
-    model = model.LSTM(max(word_bank.values()) + 1, 256)
-    model.load_state_dict(torch.load(PATH, weights_only=True, map_location=torch.device('cpu')))
-    model.eval()
-    with torch.no_grad():
-        while True:
-            choice = input(
-                '''Use this LSTM to predict whether IMDB reviews are positive or negative.
-                (Enter) Predict sentiment of IMDB review 
-                (Q) Exit
-                ''')
-            if choice == 'Q':
-                print('Goodbye!')
-                break
-            review = input('Enter review: ')
-            prediction = model(data_pipeline(review)).item()
-            threshold = 0.5
-            print('Prediction value : {:.2f}'.format(prediction))
-            if prediction > threshold:
-                print('The model predicts a positive review')
-            else:
-                print('The model predicts a negative review')
-
->>>>>>> Stashed changes
         
--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -60,67 +59,4 @@ class LSTM(nn.Module):
         c = torch.mul(f, cprev) + torch.mul(i, candidate_c)
         output = torch.sigmoid(self.output_gate(embedded))
         h = torch.mul(output, self.tanh(c))
-=======
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-
-class Reviews(torch.utils.data.Dataset):
-    def __init__(self, df):
-        self.reviews = df['tokenizedReview'].tolist()
-        self.labels = df['sentiment'].tolist()
-
-    def __len__(self):
-        return len(self.reviews)
-
-    def __getitem__(self, i):
-        return self.reviews[i], float(self.labels[i])
-
-
-class LSTM(nn.Module):
-
-    def __init__(self, vocab_size, hidden_size, embedded_size=None):
-        if embedded_size == None:
-            embedded_size = hidden_size
-        self.hidden_size = hidden_size
-        super(LSTM, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedded_size, padding_idx=0)
-        self.model_y = nn.Linear(self.hidden_size, 1)
-        self.dropout = nn.Dropout(p=0.2)
-        
-        self.forget = nn.Linear(embedded_size+self.hidden_size, self.hidden_size)
-        self.input_gate = nn.Linear(embedded_size+self.hidden_size, self.hidden_size)
-        self.candidate = nn.Linear(embedded_size+self.hidden_size, self.hidden_size)
-        self.output_gate = nn.Linear(embedded_size+self.hidden_size, self.hidden_size)
-        
-        self.tanh = nn.Tanh()
-        
-        nn.init.constant_(self.forget.bias, 1.0)
-        
-    def forward(self, tokens):
-        batch_size, seq_length = tokens.shape
-        h = torch.zeros(batch_size, self.hidden_size, device=device)
-        c = torch.zeros(batch_size, self.hidden_size, device=device)
-        for i in range(0, seq_length):
-            x = tokens[:, i]
-            mask = (x != 0).unsqueeze(1).float()
-            h_new, c_new = self.lstm_block(x, h, c)
-            h_new, c_new = self.lstm_block(x, h, c)
-            h = mask * h_new + (1 - mask) * h
-            c = mask * c_new + (1 - mask) * c
-        h = self.dropout(h)
-        y = self.model_y(h)
-        return torch.sigmoid(y)
-
-    def lstm_block(self, x, hprev, cprev):
-        embedded = torch.cat([self.embedding(x), hprev], dim=1)
-        f = torch.sigmoid(self.forget(embedded))
-        i = torch.sigmoid(self.input_gate(embedded))
-        candidate_c = self.tanh(self.candidate(embedded))    
-        c = torch.mul(f, cprev) + torch.mul(i, candidate_c)
-        output = torch.sigmoid(self.output_gate(embedded))
-        h = torch.mul(output, self.tanh(c))
->>>>>>> Stashed changes
         return h, c
